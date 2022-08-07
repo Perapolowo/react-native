8b48687878e4ceb0413394cbf0c64a20f9209098
Nattapol Singhad <disntp_2001@hotmail.com>
2022-08-07 16:31:09 +0700

    first commit

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    console.log("I'm Pressing")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={handlePress}>Open up App.js to start working on your app! eiei</Text>
       <StatusBar style="auto" />
     </View>
   );
